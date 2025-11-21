0db424304a5f135da72175360849cac5223d6c8e
alyxBenAtmo <alyxandre.beniahia@atmosud.org>
2025-11-21 16:25:34 +0100

    :lipstick: Modif bouton de controle de fond de carte en bouton radio

--- src/components/controls/BaseLayerControl.tsx
+++ src/components/controls/BaseLayerControl.tsx
@@ -104,24 +104,14 @@ const BaseLayerControl: React.FC<BaseLayerControlProps> = ({
                 }`}
               >
                 <div
-                  className={`w-3 h-3 rounded border mr-2 flex items-center justify-center ${
+                  className={`w-3 h-3 rounded-full border mr-2 flex items-center justify-center ${
                     currentBaseLayer === layerKey
-                      ? "bg-blue-600 border-blue-600"
+                      ? "border-blue-600"
                       : "border-gray-300/50"
                   }`}
                 >
                   {currentBaseLayer === layerKey && (
-                    <svg
-                      className="w-2 h-2 text-white"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    <div className="w-1.5 h-1.5 rounded-full bg-blue-600" />
                   )}
                 </div>
                 <span>{layerKey}</span>
